5fdfa9ae0f0c6d0ac020c5c750616030d6c1c8aa
Antonio <apln2@ci.ufpe.br>
2021-05-06 02:34:21 -0300

    completed implementation

--- monte_carlo/agent.py
+++ monte_carlo/agent.py
@@ -2,34 +2,28 @@
 
 import gym
 import numpy
+from collections import defaultdict
 
 class MonteCarloBlackjackAgent:
-    def __init__ (self, seed = None, initial_draw_prob=0.5):
+    def __init__ (self, seed = None):
         self.__env = gym.make('Blackjack-v0')
         self.__env.seed(seed)
-        self.__state_value_table = numpy.zeros([10, 10, 2])
-        self.__policy_table = numpy.full([10, 10, 2], initial_draw_prob)
-        self.__rewards = {}
         
-        self.__state_action_value_table = numpy.zeros([10, 10, 2, 2])
+        self.__value_table = defaultdict(lambda: numpy.zeros(2))
+        
+        self.__reward_sum = defaultdict(float)
+        self.__visits = defaultdict(int)
+
     
     @property
-    def state_value_table(self):
-        return self.__state_value_table
+    def policy(self):
+        return self.__value_table
     
-    @property
-    def policy_table(self):
-        return self.__policy_table
+    def evaluate(self, state):
+        return self.__value_table[state]
     
-    @property
-    def rewards(self):
-        return self.__rewards
     
-    @property
-    def q(self):
-        return self.__policy_table
-    
-    def generate_random_episode(self):
+    def __generate_episode(self, policy):
         '''
         Generates random episodes based on the current policy
         '''
@@ -39,55 +33,32 @@ class MonteCarloBlackjackAgent:
         while (not done):
             state_prime = state
             # prob = self.evaluate(state)
-            probs = [0.8, 0.2] if state[0] < 20 else [0.2, 0.8]
-            action = numpy.random.choice([1, 0], p=probs)
+            probs = policy(state)
+            action = numpy.random.choice([0, 1], p=probs)
             state, reward, done, _ = self.__env.step(action)
-            if (state[0] > 11):
-                episode.append((state_prime, action, reward))
-        return episode, state
+            episode.append((state_prime, action, reward))
+        return episode
         
-    
-    def evaluate(self, state: tuple):
-        '''
-        Evaluates a state to get the probability of the player choosing to hit
-        if the player holds cards that make up to a sum of 11 or less, the
-        choice is always to hit
-        
-        state (tuple)
-        returns (float)
-        '''
-        if state[0] < 12: return 1
-        state_index = (state[0]-12, state[1]-1, int(state[2]))
-        return self.policy_table[state_index]
-    
-    def update_value_table(self, state: tuple, value):
-        state_index = (state[0]-12, state[1]-1, int(state[2]))
-        self.state_value_table[state_index] = value
-    
-    def update_policy_table(self, state: tuple, value):
-        state_index = (state[0]-12, state[1]-1, int(state[2]))
-        self.policy_table[state_index] = value
-    
-    def update_q(self, state: tuple, action: int, value):
-        state_index = (state[0]-12, state[1]-1, int(state[2]), action)
-        self.__state_action_value_table[state_index] = value
-        
-    @staticmethod
-    def average_list(l: list):
-        return sum(l)/len(l)
 
-    def learn (self, episode, gamma = 1, epsilon = 0.01):
-        G = 0
-        for state, action, reward in episode[::-1]:
-            G = gamma * G + reward
-            if ((state, action) in list((x[0], x[1]) for x in episode)):
-                if not (state, action) in self.rewards:
-                    self.rewards[(state, action)] = []
-                self.rewards[(state, action)].append(G)
-                value = self.average_list(self.rewards[(state, action)])
-                self.update_q(state, action, value)
-                state_index = (state[0]-12, state[1]-1, int(state[2]))
-                max_a = numpy.argmax(self.__state_action_value_table[state_index])
-                # update_value = (max_a + (1 -2*max_a)*(epsilon/2))
-                # self.update_policy_table(state, update_value)
-                self.update_policy_table(state, max_a)
+    def learn (self, policy, gamma = 1):
+        while (True):
+            episode = self.__generate_episode(policy)
+            G = 0
+            for i, step in enumerate(episode[::-1]):
+                state, action, reward = step
+                pair = (state, action)
+                G = gamma * G + reward
+                if (pair not in [(x[0], x[1]) for x in episode][i::-1][1:]):
+                    self.__visits[pair] += 1
+                    self.__reward_sum[pair] += G
+                    self.__value_table[state][action] = \
+                        self.__reward_sum[pair] / self.__visits[pair]
+            yield
+
+def generate_epsilon_greedy_policy(policy, epsilon):
+    def policy_greedy(state):
+        prob = numpy.ones(2, dtype=float) * epsilon / 2
+        best_action = numpy.argmax(policy[state])
+        prob[best_action] += 1. - epsilon
+        return prob
+    return policy_greedy
--- monte_carlo/main.py
+++ monte_carlo/main.py
@@ -1,16 +1,55 @@
 #!/bin/env python3
-
+import numpy
+import matplotlib
 import matplotlib.pyplot as plt
-from agent import MonteCarloBlackjackAgent
+from collections import defaultdict
+from agent import MonteCarloBlackjackAgent, generate_epsilon_greedy_policy
+
+def plot_value_function(V, title = "Value Function"):
+    '''
+    Plots the value function as a surface plot.
+    '''
+    min_x = 11 # min(k[0] for k in V.keys())
+    max_x = max(k[0] for k in V.keys())
+    min_y = min(k[1] for k in V.keys())
+    max_y = max(k[1] for k in V.keys())
+    
+    x_range = numpy.arange(min_x, max_x + 1)
+    y_range = numpy.arange(min_y, max_y + 1)
+    X, Y = numpy.meshgrid(x_range, y_range)
+
+    # Find value for all (x, y) coordinates
+    Z_noace = numpy.apply_along_axis(lambda _: V[(_[0], _[1], False)], 2, numpy.dstack([X, Y]))
+    Z_ace = numpy.apply_along_axis(lambda _: V[(_[0], _[1], True)], 2, numpy.dstack([X, Y]))
+
+    def plot_surface(X, Y, Z, title):
+        fig = plt.figure(figsize = (20, 10))
+        ax = fig.add_subplot(111, projection = '3d')
+        surf = ax.plot_surface(X, Y, Z, rstride = 1, cstride = 1,
+                               cmap = matplotlib.cm.coolwarm, vmin = -1.0, vmax = 1.0)
+        ax.set_xlabel('Player Sum')
+        ax.set_ylabel('Dealer Showing')
+        ax.set_zlabel('Value')
+        ax.set_title(title)
+        ax.view_init(ax.elev, -120)
+        fig.colorbar(surf)
+        plt.show()
+
+    plot_surface(X, Y, Z_noace, "{} (No Usable Ace)".format(title))
+    plot_surface(X, Y, Z_ace, "{} (Usable Ace)".format(title))
 
 if __name__ == '__main__':
     agent = MonteCarloBlackjackAgent()
     
-    for i in range(1000):
-        episode, _ = agent.generate_random_episode()
-        agent.learn(episode)
-        
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    X, Y, Z = agent.q.nonzero()
-    ax.plot_trisurf(X, Y, Z, vmin=-1.0, vmax=1.0)
+    random_policy = defaultdict(lambda: numpy.zeros(2))
+    p = generate_epsilon_greedy_policy(random_policy, epsilon = 0.1)
+    test = agent.learn(p)
+    
+    for _ in range(500000):
+        next(test)
+        if (_+1 in [50000, 100000, 500000]):    
+            V = defaultdict(float)
+            for state, actions in agent.policy.items():
+                V[state] = numpy.max(actions)
+
+            plot_value_function(V, title = 'Optimal Value Function')
